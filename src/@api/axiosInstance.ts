eec6b15f7485ffc0e0fa0f262d13fc9dafe9a354
seonnn <ssni0105@gmail.com>
2023-08-12 23:53:18 +0900

    Feat: axios interceptors 설정 추가

--- src/@api/axiosInstance.ts
+++ src/@api/axiosInstance.ts
@@ -1,5 +1,12 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { handleAxiosDates } from 'src/@utils/date.utils';
 
-export const axiosInstance = axios.create({
+export const api_ = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL}`,
+});
+
+api_.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = localStorage.getItem('token');
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
