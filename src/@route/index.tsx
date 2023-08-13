dcd46aa3571bf12dbfc3b4401ae562ccb35201e2
seonnn <ssni0105@gmail.com>
2023-08-13 23:30:31 +0900

    Feat: 관리자 로그인 페이지 route 연결

--- src/@route/index.tsx
+++ src/@route/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import LoginPage from 'src/@service/admin/pages/LoginPage';
 
 const RootRouter = () => {
   return (
     <Routes>
-      <Route path="" element={<div></div>} />
+      <Route path="/admin">
+        <Route path="login" element={<LoginPage />}></Route>
+      </Route>
     </Routes>
   );
 };
